af907899067939f1eaf6a04e0b95cb16c83d017c
Mauritz <mauritzz@kth.se>
2015-12-02 00:34:32 +0100

    Added grunt

---
+++ gruntfile.js
@@ -0,0 +1,26 @@
+﻿// gruntfile.js
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        less: {
+            dev: {
+                options: {
+                    paths: ["static/css"]
+                },
+                files: {
+                    "static/css/compiled.css": "static/css/import.less"
+                }
+            }
+        },
+        watch: {
+            files: ['<%= less.files %>'],
+            tasks: ['less']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['less']);
+
+};
