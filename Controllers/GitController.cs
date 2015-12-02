2be210724de87455d5d1a6912b6a6b3aa19ae576
Mauritz <mauritzz@kth.se>
2015-12-02 20:02:54 +0100

    Initial commit of admin controller

---
+++ Controllers/GitController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace aurora.Controllers
+{
+    [Route("/Backoffice/")]
+    public class GitController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
