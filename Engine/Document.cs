9dc30451fa578932927443aa3d25de5de37636ca
Mauritz <mauritzz@kth.se>
2015-12-02 20:02:46 +0100

    Initial commit of Engine package

---
+++ Engine/Document.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace docs.Engine
+{
+    public class Document
+    {
+        public string Title { get; private set; }
+        public string Body { get; private set; }
+
+        public Document(string Title, string Body)
+        {
+
+        }
+    }
+}
---
+++ Engine/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace docs.Engine
+{
+    public class Engine
+    {
+        private static Engine instance = null;
+
+        public Engine getInstance()
+        {
+            if (instance == null)
+                instance = new Engine();
+
+            return instance;
+        }
+    }
+}
