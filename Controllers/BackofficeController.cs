76bd8608fa9fdfc76e7b98ca3ae53e598ab0ba34
Mauritz <mauritzz@kth.se>
2015-12-04 22:43:44 +0100

    First untested merge of /Clone and /rea

--- Controllers/BackofficeController.cs
+++ Controllers/BackofficeController.cs
@@ -12,7 +12,7 @@ namespace aurora.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            var root = new DirectoryInfo(Directory.GetCurrentDirectory());
+            var root = new DirectoryInfo(Directory.GetCurrentDirectory() + "/repos");
             ViewData["Title"] = "Backoffice";
             ViewData["RepoFolder"] = Engine.RepoFolder;
             ViewData["RepoURL"] = Engine.RepoURL;
@@ -24,11 +24,44 @@ namespace aurora.Controllers
             return View("Backoffice");
         }
 
-        [Route("Clone")]
-        public async Task Clone()
+        [Route("Update")]
+        public async void Clone()
         {
-            Engine.Now = DateTime.Now;
-            await Task.Factory.StartNew(() => LibGit2Sharp.Repository.Clone(Engine.RepoURL, Engine.RepoFolder));
+            var repo = "repos/" + DateTime.Now.ToString("YYYYMMddhhmmss");
+            await Task.Factory.StartNew(() => LibGit2Sharp.Repository.Clone(Engine.RepoURL, repo));
+
+            var folder = new DirectoryInfo(repo);
+
+            Engine.RootDoc = new Document("Dokumentation", "docs");
+
+            // For all directories
+            foreach (var d in folder.GetDirectories())
+            {
+                // Create a root document
+                var slug = Regex.Replace(d.Name, "[^A-Za-z0-9-]", "").ToLower();
+                var doc = new Document(d.Name, slug);
+                var children = doc.Children;
+
+                foreach (var f in d.GetFiles())
+                {
+                    var content = new StreamReader(f.FullName).ReadToEnd();
+
+                    // Fill the root docuement
+                    if (f.Name.ToLower().Equals("index.md"))
+                    {
+                        doc.Body = content;
+                    }
+                    // Or append it with sub-documents
+                    else
+                    {
+                        doc.Children.Add(new Document(f.Name, slug, content));
+                    }
+                }
+
+                Engine.RootDoc.Children.Add(doc);
+            }
+
+
             Response.Redirect("/Backoffice/");
         }
 
@@ -36,11 +69,12 @@ namespace aurora.Controllers
         public void Read()
         {
             string repoFolder = Engine.RepoFolder;
+            if (repoFolder.Equals("repo0"))
+                Response.Redirect("/Backoffice/?err");
+
             var repo = new DirectoryInfo(repoFolder);
             Engine.RootDoc = new Document("Dokumentation", "docs");
 
-            if (repoFolder.Equals("repo0"))
-                Response.Redirect("/Backoffice/?err");
 
             // For all directories
             foreach (var d in repo.GetDirectories())
