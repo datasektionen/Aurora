dbdc4677a048ade6d00057399b16ea0cc0de8514
Mauritz <mauritzz@kth.se>
2015-12-05 23:32:36 +0100

    Appropriate 404 handling, data changes

--- Controllers/DocsController.cs
+++ Controllers/DocsController.cs
@@ -9,11 +9,14 @@ namespace aurora.Controllers
         [Route("/")]
         public IActionResult Index()
         {
+            // Populate ViewData and render
             ViewData["Title"] = Data.RootDoc.Title;
             ViewData["Body"] = Data.RootDoc.Body;
             ViewData["Children"] = Data.RootDoc.Children;
+            ViewData["ParentTitle"] = Data.RootDoc.Title;
+            ViewData["Slug"] = Data.RootDoc.Slug;
             ViewData["IsRoot"] = true;
-            ViewData["Slug"] = "/docs";
+            ViewData["Path"] = "/docs";
 
             return View("Document");
         }
@@ -21,16 +24,20 @@ namespace aurora.Controllers
         [Route("/docs/{section}")]
         public IActionResult Section(string section)
         {
-            var doc = Data.RootDoc.Children.Where(d => d.Slug.Equals(section)).First();
-
-            if (doc == null)
+            // Try to fetch document with the desired slug, /404 otherwise
+            var getDoc = Data.RootDoc.Children.Where(d => d.Slug.Equals(section));
+            if (getDoc.Count() != 1)
                 return Redirect("/404");
+            var doc = getDoc.First();
 
+            // Populate ViewData and render
             ViewData["Title"] = doc.Title;
             ViewData["Body"] = doc.Body;
             ViewData["Children"] = doc.Children;
+            ViewData["ParentTitle"] = doc.Title;
+            ViewData["Slug"] = doc.Slug;
             ViewData["IsRoot"] = false;
-            ViewData["Slug"] = "/docs/" + doc.Slug;
+            ViewData["Path"] = "/docs/" + doc.Slug;
 
             return View("Document");
         }
@@ -38,19 +45,26 @@ namespace aurora.Controllers
         [Route("/docs/{section}/{article}/")]
         public IActionResult Article(string section, string article)
         {
-            var sec = Data.RootDoc.Children.Where(s => s.Slug.Equals(section)).First();
-            if (sec == null)
+            // Try to fetch section with the desired slug, /404 otherwise
+            var getSec = Data.RootDoc.Children.Where(s => s.Slug.Equals(section));
+            if (getSec.Count() != 1)
                 return Redirect("/404");
+            var sec = getSec.First();
 
-            var art = sec.Children.Where(d => d.Slug.Equals(article)).First();
-            if (art == null)
+            // Try to fetch article with the desired slug, /404 otherwise
+            var getArt = sec.Children.Where(d => d.Slug.Equals(article));
+            if (getArt.Count() != 1)
                 return Redirect("/404");
+            var art = getArt.First();
 
+            // Populate ViewData and render
             ViewData["Title"] = art.Title;
             ViewData["Body"] = art.Body;
             ViewData["Children"] = sec.Children;
+            ViewData["ParentTitle"] = sec.Title;
+            ViewData["Slug"] = art.Slug;
             ViewData["IsRoot"] = false;
-            ViewData["Slug"] = "/docs/" + sec.Slug;
+            ViewData["Path"] = "/docs/" + sec.Slug;
 
             return View("Document");
         }
