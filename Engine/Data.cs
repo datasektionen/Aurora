c131a5dea5d9fa5da51c2c55092c648756706671
Mauritz <mauritzz@kth.se>
2015-12-05 03:42:54 +0100

    Fixes for Data.c

--- Engine/Data.cs
+++ Engine/Data.cs
@@ -2,7 +2,16 @@
 {
     public class Data
     {
+        /// <summary>
+        /// The git repo to clone.
+        /// </summary>
         public static string RepoURL = "https://github.com/datasektionen/Docs.git";
-        public static Document RootDoc = new Document("Dokumentation", "docs");
+
+        /// <summary>
+        /// Root document container, carries the front page as well as
+        /// pointers to child nodes.
+        /// </summary>
+        public static Document RootDoc = 
+            new Document("docs", "Dokumentation", "Dokumentationen har inte lästs in ännu.");
     }
 }
