a34b30506509d5c9d9e17363161f976bc7427dc3
Mauritz <mauritzz@kth.se>
2016-03-19 04:13:49 +0100

    Reworked node server

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-var express = require('express');
-var app = express();
+// Require libs and set up port
+var http = require('http'), fs = require('fs');
+const PORT = process.env.PORT || 8000;
 
-app.use(express.static(__dirname));
-app.listen('8000');
-console.log('Listening on port 8000');
+// Handle requests
+function handleRequest(req, res) {
+    if (req.url === '/test/styrdok')
+        res.end(fs.readFileSync('test/styrdok.html'));
+    else if (req.url === '/test/tech')
+        res.end(fs.readFileSync('test/tech.html'));
+    else
+        res.end(fs.readFileSync('css/compiled.css'));
+}
+
+// Start server
+http.createServer(handleRequest).listen(PORT, () => console.log("Server listening on: http://localhost:%s", PORT));
